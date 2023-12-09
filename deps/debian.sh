be742171543d4d7fee6d793d36a2aae13b5b918a
fukiame <fukiame@proton.me>
2023-12-09 10:34:01 +0700

    deps: initial
    quick installation of common dependencies for building

---
+++ deps/debian.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install llvm lld bc bison ca-certificates curl flex gcc git libc6-dev \
+  libssl-dev openssl python3 python2 zip zstd make clang gcc-arm-linux-gnueabi \
+  software-properties-common device-tree-compiler libxml2 libarchive-tools \
+  perl libelf-dev libssl-dev libtfm-dev wget xz-utils -y
+

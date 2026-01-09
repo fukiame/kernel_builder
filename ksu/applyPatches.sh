81fa9ed722ae4f00c10ed17fedfd6128c7dfc8b6
fukiame <fukiame@proton.me>
2026-01-09 15:46:31 +0700

    backslash min 4.9 v1.8
    Signed-off-by: fukiame <fukiame@proton.me>

--- ksu/applyPatches.sh
+++ ksu/applyPatches.sh
@@ -6,10 +6,10 @@ export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/$1env"
 
-curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/refs/heads/main/kernel/setup.sh" | bash -
+curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/refs/heads/master/kernel/setup.sh" | bash -
 git add . && git commit -am "drivers: KernelSU"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
-KSU_ver=$(($KSU_git_ver + 10000 + 200))
+KSU_ver=$(($KSU_git_ver + 30000))
 
 patchesdir="$outside/ksu/patches/$(echo $kernel_ver | cut -d. -f1,2)"
 if [[ -d "$patchesdir" ]]; then
@@ -25,5 +25,5 @@ sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}\"/" "$
 
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
 
-echo -e " \nincludes rsuntk's KernelSU fork, ver ${KSU_ver}" >> banner_append
+echo -e " \nincludes backslashxx's KernelSU fork, ver ${KSU_ver}" >> banner_append
 

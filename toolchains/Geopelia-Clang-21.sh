73829fa0845828f2e7c7c7002ed9aa95029028e8
fukiame <fukiame@proton.me>
2026-01-21 11:49:47 +0700

    toolchains: Geopelia-Clang-21: bump to 21.1.8
    Signed-off-by: fukiame <fukiame@proton.me>

--- toolchains/Geopelia-Clang-21.sh
+++ toolchains/Geopelia-Clang-21.sh
@@ -10,7 +10,7 @@ case $1 in
     # Clone compiler
     if [[ ! -d "${dir}" ]]; then
       mkdir ${dir} && cd ${dir}
-      curl -Lo a.tar.gz "https://github.com/fukiame/Geopelia-Clang/releases/download/Geopelia-Clang-21.1.7/Geopelia-Clang-21.1.7.tar.gz"
+      curl -Lo a.tar.gz "https://github.com/fukiame/Geopelia-Clang/releases/download/Geopelia-Clang-21.1.8/Geopelia-Clang-21.1.8.tar.gz"
       tar -zxf a.tar.gz
     fi
   ;;

a9cb4fb907c6319a70e70015e2934dc97799cdb8
fukiame <fukiame@proton.me>
2026-02-14 01:59:28 +0700

    /kill @e[type=shellcheck:warn]
    Signed-off-by: fukiame <fukiame@proton.me>

--- ksu/applyPatches.sh
+++ ksu/applyPatches.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+# shellcheck disable=SC2154,SC2155,SC1090
 # hdjsjfjjwufbeizihfjejzf
 
 export maindir="$(pwd)"
@@ -9,12 +10,12 @@ source "${outside}/$1env"
 curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/refs/heads/master/kernel/setup.sh" | bash -
 git add . && git commit -am "drivers: KernelSU"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
-KSU_ver=$(($KSU_git_ver + 30000))
+KSU_ver=$((KSU_git_ver + 30000))
 
 if [ "$ksu_use_tamper" == "true" ] ; then
   echo "CONFIG_KSU_TAMPER_SYSCALL_TABLE=y" >> "${defconfig_file}"
 else
-patchesdir="$outside/ksu/patches/$(echo $kernel_ver | cut -d. -f1,2)"
+patchesdir="$outside/ksu/patches/$(echo "$kernel_ver" | cut -d. -f1,2)"
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
     git am "$patch_file"
@@ -27,7 +28,7 @@ fi
 
 sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}\"/" "${defconfig_file}"
 
-echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
+grep 'CONFIG_LOCALVERSION=' "${defconfig_file}"
 
 echo -e " \nincludes backslashxx's KernelSU fork, ver ${KSU_ver}" >> banner_append
 

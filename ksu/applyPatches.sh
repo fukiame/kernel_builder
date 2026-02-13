e7865f9777ce4ee231a9a97f36fff6fa21f76f2d
fukiame <fukiame@proton.me>
2026-02-14 01:55:24 +0700

    tamper is 4 lines
    Signed-off-by: fukiame <fukiame@proton.me>

--- ksu/applyPatches.sh
+++ ksu/applyPatches.sh
@@ -11,6 +11,9 @@ git add . && git commit -am "drivers: KernelSU"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 30000))
 
+if [ "$ksu_use_tamper" == "true" ] ; then
+  echo "CONFIG_KSU_TAMPER_SYSCALL_TABLE=y" >> "${defconfig_file}"
+else
 patchesdir="$outside/ksu/patches/$(echo $kernel_ver | cut -d. -f1,2)"
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
@@ -19,6 +22,7 @@ if [[ -d "$patchesdir" ]]; then
 else
   echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
   exit 1
+fi
 fi
 
 sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}\"/" "${defconfig_file}"

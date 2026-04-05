e717f99f49ef2a68784f810d19354e6e0e35fc39
fukiame <fukiame@proton.me>
2026-04-05 19:32:20 +0700

    ksu fork count > my braincell count
    Signed-off-by: fukiame <fukiame@proton.me>

--- ksu/applyPatches.sh
+++ ksu/applyPatches.sh
@@ -6,10 +6,10 @@ export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/$1env"
 
-curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/refs/heads/main/kernel/setup.sh" | bash -
+curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/refs/heads/susfs-ksud/kernel/setup.sh" | bash susfs-ksud
 git add . && git commit -am "drivers: KernelSU"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
-KSU_ver=$(($KSU_git_ver + 10000 + 200))
+KSU_ver=$(($KSU_git_ver + 30000 + 200))
 
 patchesdir="$outside/ksu/patches/$(echo $kernel_ver | cut -d. -f1,2)"
 if [[ -d "$patchesdir" ]]; then
